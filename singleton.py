1fe26e24b0632f22b0d1d9e7d7789d0d057fd680
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-09-26 17:07:41 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -4,3 +4,6 @@ class Human:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
+
+
+# Добавить остальные паттерные проектирования которые я знаю
