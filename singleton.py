832474fea79c2ab97a3d42dabb7e1b6572bb6c93
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-10-03 17:04:27 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -1,7 +1,7 @@
 class Singleton:
     """Класс для демонстрации паттерна проектирования Singleton"""
     __instance = None  # В этой переменной будет храниться новый объект
-    def __new__(cls, *args, **kwargs):  # перегружаем метод __new__ от родительского класса object
+    def __new__(cls, *args):  # перегружаем метод __new__ от родительского класса object
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
