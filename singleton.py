2d5283e7fc911fa7e0413a3fe46fb9c171336eea
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-10-05 16:58:08 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -11,8 +11,29 @@ class Singleton:
 
 
 
+# StandardScaler
+from sklearn.preprocessing import StandardScaler
 
+s_train_data = np.array([[10, 20, 30], [40, 50, 60], [70, 80, 90]], dtype='int8')
+s_test_data = np.array([[1, 2, 3]])
 
+scaler = StandardScaler()  # Создаём объект класса 
+
+scaler.fit(s_train_data)  # Обучение. На этом этапе вычисялем статистики. ПРИ ЭТОМ САМИ ДАННЫЕ НЕ МЕНЯЮТСЯ
+
+print(scaler.transform(s_test_data))  # Преобразование тестовых данных
+
+# MinMaxScaler (RobustScaler по аналогии)
+from sklearn.preprocessing import MinMaxScaler
+
+mms_train_data = np.array([[13430, 45532, 1437], [14345, 434243, 1843], [17, 32434, 64436]], dtype='int8')
+mms_test_data = np.array([[113, 654, 313]])
+
+mms = MinMaxScaler()  # Создаём объект класса
+
+mms.fit(mms_train_data)  # Обучение. На этом этапе вычисляем статистики. ПРИ ЭТОМ САМИ ДАННЫЕ НЕ МЕНЯЮТСЯ
+
+print(mms.transform(mms_test_data))  # Преобразование тестовых данных
 
 
 
