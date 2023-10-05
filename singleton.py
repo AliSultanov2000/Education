4abae7717b66c59685668bd19a6c5b8c9195ea0d
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-10-05 16:57:50 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -6,39 +6,15 @@ class Singleton:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
 
-class Fabric:
-    """Класс для демонстрации паттерна проектирования Фабрика"""
-    def __init__(self, f1, f2):
-        self.f1 = f1
-        self.f2 = f2 
-    
-
-
-
-
-
-
-class Builder:
-    """Класс для демонстрации паттерна проектирования Строитель"""
-    def __init__(self, name, surname, age):
-        self.name = name
-        self.surname = surname
-        self.age = age
 
     
 
-class Human:
-    def __init__(self, name, surname, age, city):
-        self.name = name
-        self.surname = surname
-        self.age = age
-        self.city = city
 
-  def __str__(self):
-      return f'Объект с названием {self.name} {self.surname}'
 
- def __len__(self):
-     return len([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+
+
+
 
 
 
