78992f9b5227c379250b7f97099ee444accf4c94
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-10-19 17:18:42 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -5,22 +5,3 @@ class Singleton:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
-
-
-    
-
-    
-
-
-def inverse_func(X):
-    return X - 30  # Функция инверсии
-
-transformer2 = FunctionTransformer(transf_func, inverse_func=inverse_func)  # Оборачиваем кастомную функцию transf_func в ООП класс FunctionTransformer
-
-pipocka5 = Pipeline([('transform', transformer2), ('norm', MinMaxScaler())])
-pipocka5.fit(df100)  # Вычисление статистик для MinMaxScaler
-pipocka5.inverse_transform(np.array([[0.  ],
-                                     [0.25],
-                                     [0.5 ],
-                                     [0.75],
-                                     [1.  ]]))
