d9008655b8e0dc06856d0d74e8212fcb0c968f72
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-10-20 16:25:00 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -5,19 +5,3 @@ class Singleton:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
-
-
-from sklearn.feature_extraction.text import CountVectorizer 
-
-count_vectorizer1 = CountVectorizer()  # Реализует концепцию мешка слов
-vectors1 = count_vectorizer1.fit_transform(train_docs)  # Сначала запускается метод fit, а далее запускается метод transform, который return-ит вектор для каждого текста; При вызове fit получается вокабуляр слов
-vectors1
-
-# Посмотрим вокабуляр слов (на этих словах обучались). Индекс означает порядок слова в df 
-count_vectorizer1.vocabulary_
-
-
-# Пример predict-а, в нижних примерах можно также реализовать!
-predict = count_vectorizer1.transform(['Sun is sweet'])
-print(type(predict))  # Также sparse_matrix
-print(predict.toarray())
