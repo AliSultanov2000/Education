8165fb21003d92c680ccc1aee5bb40791acf5098
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-10-26 16:45:13 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -5,3 +5,16 @@ class Singleton:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
+
+
+# Вставить сюда пример кода
+
+from sklearn.impute import SimpleImputer
+
+
+imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+example_data = np.array([[np.nan, 1, 2],
+                            [3, np.nan, 4],
+                            [5, 6, np.nan]])
+
+imp.fit_transform(example_data)
