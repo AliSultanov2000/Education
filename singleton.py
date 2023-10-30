606fac534f64764a27b750c5b3180b5e81cf7e76
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-10-30 17:18:20 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -5,3 +5,27 @@ class Singleton:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import FunctionTransformer
+
+
+X = pd.DataFrame({'age': [10, 20, 30, 40], 'balance': [400, 600, 800, 1000], 'kernel': ['White', 'Black', 'Green', 'Yellow'], 'sex': ['male', 'female', 'male', 'female']})
+
+
+
+def change_num1(X: pd.DataFrame, y=None):
+    """Функция для работы с колонкой age"""
+    return X + 10 
+
+def change_num2(X: pd.DataFrame, y=None):
+    """Функция для работы с колонкой balance"""
+    return X - 100
+
+
+trans1 = FunctionTransformer(change_num1)
+trans2= FunctionTransformer(change_num2)
+
+
+ct = ColumnTransformer([("tr1", trans1, ["age"]), ("tr2", trans2, ["balance"]), ("tr3", OneHotEncoder(), ['kernel', 'sex'])])
+ct.fit_transform(X)
