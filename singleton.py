08c2bedea266226032d320c1d593e0a74fcac005
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-02 17:09:49 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -32,6 +32,35 @@ ar, y = make_classification(n_samples=10000, n_features=3, n_informative=2,
                            flip_y=0, n_clusters_per_class=2)
 
 
+# In this case we can use Pipeline (inner and outer), ColumnTransformer, custom classes, custom def function in Pipeline, bu we selected simple example to show OptunaSearchCV
+
+# KFold strategy
+kf = KFold(n_splits=5, shuffle=True, random_state=50)
+
+
+# Define the model
+clf = CatBoostClassifier(verbose=False)
+
+
+# Define param distribution
+# IMPORTANT: IN param_distrs we can use only optuna.distribution! For instance, we can't use list, np.array and e.t.c.
+param_distrs = {
+                'min_data_in_leaf': optuna.distributions.IntDistribution(1, 10),
+                'iterations': optuna.distributions.IntDistribution(800, 1200, 100),
+                }
+
+
+# OptunaSearchCV 
+opt_search = optuna.integration.OptunaSearchCV(clf,
+                                               param_distrs,
+                                               cv=kf,
+                                               scoring='f1',
+                                               n_trials=10,  # Important parameters! In total we have 10 combination of hyperparameters and it's all
+                                               timeout=100)  # Important parameters! In total trial time = 100 second
+
+
+# Let's get started 
+opt_search.fit(X_train, y_train)
 # DataFrame, label
 df = pd.DataFrame(ar, columns = ['feat1', 'feat2', 'feat3'])
 df['y'] = y
