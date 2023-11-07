04cfb11eb5e6c634acde1f9cd841c61047ff1f06
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-07 16:41:38 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -7,37 +7,4 @@ class Singleton:
         return cls.__instance
 
 
-def stream_detection(video_path: str) -> None:
-    """Run inference and plot the result for video data"""
-    
-    cap = cv2.VideoCapture(video_path)
 
-    # Loop through the video frames
-    while cap.isOpened():
-    # Read a frame from the video
-        success, frame = cap.read()
-
-        if success:
-            # Run YOLOv8 inference on the frame
-            results = model(frame)
-
-            # Visualize the results on the frame
-            annotated_frame = results[0].plot()
-
-            # Display the annotated frame
-            cv2.imshow("YOLOv8 Inference", annotated_frame)
-
-            # Break the loop if 'q' is pressed
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
-        else:
-            # Break the loop if the end of the video is reached
-            break
-
-    # Release the video capture object and close the display window
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-# Test
-stream_detection(r"C:\Users\1NR_Operator_33\Downloads\Поток машин на ТТК (Москва), весна-лето 2018.mp4")
