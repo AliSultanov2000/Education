25eb800f5c3635d730576582958a1f9bc0d7d927
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-13 16:56:25 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -5,18 +5,3 @@ class Singleton:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
-
-
-plt.bar([1, 2, 3], [10, 20, 30],
-        width=0.45,
-        alpha=0.6,  # Прозрачность,
-        color = ['red', 'green', 'blue']
-        )
-
-plt.title('Simple barplot')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.xticks([1, 2, 3])
-plt.yticks([10, 20, 30, 40])
-plt.show()
-plt.show()
