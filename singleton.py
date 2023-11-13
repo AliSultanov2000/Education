ad058764beb6b350035c0a34e07e4edac3d6070c
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-13 16:55:12 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -5,3 +5,19 @@ class Singleton:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
+
+plt.figure(figsize=(7, 5))
+
+x = range(20)  
+y = [i ** 2 for i in x]
+
+plt.plot(x, y,
+        label='quadratic function',  
+        marker='o', markersize=4)
+
+plt.title('Example of graph')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid()
+plt.legend()
+plt.show()
