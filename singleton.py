92cb5485eca3548a0bf2f729070acf74e03cf259
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-13 16:55:36 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -6,46 +6,27 @@ class Singleton:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
 
-plt.figure(figsize=(7, 5))
 
-x = range(20)  
-y = [i ** 2 for i in x]
+plt.figure(figsize=(10, 5))
 
-plt.plot(x, y,
-        label='quadratic function',  
-        marker='o', markersize=4)
-
-plt.title('Example of graph')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid()
-plt.legend()
-plt.show()
-
-
-plt.figure(figsize=(7, 5))
-
-x = range(11)  
-y1 = [i for i in x]  
-y2 = [i ** 2 for i in x]
-
+plt.subplot(1, 2, 1)
 plt.plot(x, y1,
          label='linear function',
-         linewidth=1.5,
          marker='*', markersize=4,
          color='red')
 
+plt.title('Linear function')
+plt.xlabel('x', fontsize=14)
+plt.ylabel('y', fontsize=14)
+plt.grid()
+
+plt.subplot(1, 2, 2)
 plt.plot(x, y2,
-        label='quadratic function',
-        linewidth=1.5,  
+        label='quadratic function',  
         marker='o', markersize=4,
         color='green')
 
-plt.title('Example of simple graphs')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.xlim(0, 10)
-plt.ylim(0, 15) 
+plt.title('Quadratic function')
+plt.xlabel('x', fontsize=14)
+plt.ylabel('y', fontsize=14)
 plt.grid()
-plt.legend()
-plt.show()
