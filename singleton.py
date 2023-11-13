ee828557582b2c95c2920132b50d767e4992b496
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-13 16:55:46 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -30,3 +30,19 @@ plt.title('Quadratic function')
 plt.xlabel('x', fontsize=14)
 plt.ylabel('y', fontsize=14)
 plt.grid()
+
+
+labels = ['G1', 'G2', 'G3', 'G4', 'G5']
+men_means = [20, 34, 30, 35, 27]
+women_means = [25, 32, 34, 20, 25]
+
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
+
+plt.bar(x - width/2, men_means, width, label='Men')
+plt.bar(x + width/2, women_means, width, label='Women')
+
+
+
+plt.legend()
+plt.show()
