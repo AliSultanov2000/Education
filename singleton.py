f3bc834bf88b1da755cb5dd7d8c82f947d34bb4d
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-15 17:07:51 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -5,23 +5,3 @@ class Singleton:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
-
-
-x = range(11)  
-y1 = [i for i in x]  
-y2 = [i ** 2 for i in x]
-
-fig, axs = plt.subplots(1, 2, figsize=(12, 7))
-axs[0].plot(x, y1, label='linear function', marker='*', markersize=4, color='red')
-
-axs[0].set_xlabel('Linear', fontsize=14)
-axs[0].set_ylabel('Value', fontsize=14)
-axs[0].set_title('First graph')
-
-axs[1].plot(x, y2, label='quadratic function', marker='o', markersize=4, color='green')
-
-axs[1].set_xlabel('Quadratic', fontsize=14)
-axs[1].set_ylabel('Value', fontsize=14)
-axs[1].set_title('Second graph')
-
-plt.show()
