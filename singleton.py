8ca56d221505903ac83f73960e54604e6be84332
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-16 16:32:33 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -5,32 +5,3 @@ class Singleton:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)  # Запускаем __new__ от object, в противном случае не запускаем от object
         return cls.__instance
-
-
-
-# plt.rcParamsDefault.copy()  # Просмотр существующих rcParams
-
-matplotlib_params = {
-    'legend.fontsize': 'medium',
-    'lines.linewidth': 2,
-    # 'axes.grid': False,
-    'xtick.labelsize': 13,
-    'ytick.labelsize': 13,
-    'axes.labelsize': 14,
-    'axes.titlesize': 14,
-    "legend.fancybox": True,
-    'legend.loc': 'best'
-}
-
-pylab.rcParams.update(matplotlib_params)
-
-fig, ax = plt.subplots()
-ax.set(title='Example of graph', xlabel='x', ylabel='y')
-ax.plot(x, y, label='quadratic function')
-ax.legend();
-
-fig, ax = plt.subplots()
-ax.set(title='Example of simple graphs', xlabel='x', ylabel='y', xlim=(0, 10), ylim=(0, 15))
-ax.plot(x, y1, label='linear function')
-ax.plot(x, y2, label='quadratic function')
-ax.legend();
