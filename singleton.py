136765c2cd220e78a1064521caf91089c6022aaa
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-17 15:12:29 +0300

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -58,3 +58,33 @@ example_data = np.array([[np.nan, 1, 2],
                             [5, 6, np.nan]])
 
 imp.fit_transform(example_data)
+
+class RemoveOutliers(TransformerMixin):
+    """Класс для удаления выбросов интерквартильным методом"""
+
+    def __init__(self, q_25=None, q_50=None, q_75=None):
+        """init хранятся статистики по всем признакам!"""
+        self.q_25 = q_25   
+        self.q_50 = q_50 
+        self.q_75 = q_75
+    
+
+    def fit(self, X: pd.DataFrame, y=None): 
+        """fit вычисляем статистики по всем признакам на train"""
+        self.q_25 = X.quantile(q=0.25)
+        self.q_50 = X.quantile(q=0.50)
+        self.q_75 = X.quantile(q=0.75)
+        return self
+    
+    
+    def transform(self, X: pd.DataFrame):
+        """Замена выбросов на квантили по всем признакам как на train, так и на test"""
+        for column in X.columns:
+            q_3 = self.q_75[column]
+            q_1 = self.q_25[column]
+            iqr = q_3 - q_1
+            upper_bound = q_3 + 1.5 * iqr
+            lower_bound = q_1 - 1.5 * iqr
+            X.loc[X[column] > upper_bound, column] = q_3
+            X.loc[X[column] < lower_bound, column] = q_1
+        return X
