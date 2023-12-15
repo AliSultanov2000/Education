a5c22aafd9e323873c1fe8bbf37d785da907e256
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-12-15 15:20:42 +0300

    add yolomodel

--- asyncio.py
+++ asyncio.py
@@ -177,3 +177,56 @@ if __name__ == '__main__':
     # Запуск на трекиг видеопотока
     tracker.stream_tracking(r"C:\Users\1NR_Operator_33\Downloads\танк Т90м Прорыв лучший танк в мире в бою.mp4", Display())
 
+
+class YoloModel:
+    """Базовый класс для работы с YOLOv8"""
+
+    def __init__(self, model_path):
+
+        self.device = 'cuda' if is_available() else 'cpu'
+        self.model_path = model_path
+        self.model = self.load_model()
+        self.CLASS_NAMES_DICT = self.model.names
+
+        
+    def load_model(self):
+        """Загрузка модели YOLOv8"""
+
+        model = YOLO(self.model_path)
+        model = model.to(device='cpu')
+        model.fuse()
+        return model
+
+
+    def train_model(self, data, imgsz, epochs, batch, name):
+        """Тренировка модели YOLOv8 для задачи трекинга"""
+
+        self.model.train(
+        data=data,  # Путь до YAML файла
+        name=name,  # Название модели, которая сохранится 
+        imgsz=imgsz,  
+        epochs=epochs,  
+        batch=batch)
+    
+
+    def test_model(self):
+        """Запуск на тестирование модели YOLOv8"""
+        # self.model.val()
+
+
+    def _box_plotting(self, results):
+        """Отображение на экране box-а c распознанными объектами"""
+    
+        # Получение всех боксов
+        boxes = results[0].boxes.xywh.cpu()
+        # Визуализация результата на фрейме
+        annotated_frame = results[0].plot()
+        # Отрисовка бокса с указанием id трека 
+        for box in boxes:
+            x, y, w, h = box
+            x, y, w, h = int(x), int(y), int(w), int(h)
+            # Отрисовка центров для Bounding Boxes
+            cv2.drawMarker(annotated_frame, (x, y), (0, 0, 255), cv2.MARKER_CROSS, 15, 2)
+
+        return annotated_frame 
+
