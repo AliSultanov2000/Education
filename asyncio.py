af6a9365bca03fe9ca2cce5edb371f8782a868b3
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-12-22 15:15:02 +0300

    add stacking

--- asyncio.py
+++ asyncio.py
@@ -170,3 +170,22 @@ class LogisticRegression:
 
 lr = LogisticRegression(100, 0.1, 0.001)
 lr.fit(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), np.array([1, 2, 3]))
+
+estimators = [
+     ('rf', RandomForestClassifier(n_estimators=10, random_state=42))
+     ('svr', make_pipeline(StandardScaler(),
+                           LinearSVC(dual="auto", random_state=42)))
+             ]
+
+
+skfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+clf = StackingClassifier(estimators=estimators, final_estimator=LogisticRegression(), cv=skfold)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+
+clf.fit(X_train, y_train)
+
+# Check the results on train, test
+print(f'Metric on train: {clf.score(X_train, y_train)}')
+print(f'Metric on test: {clf.score(X_test, y_test)}')
