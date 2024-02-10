d755fe42a13864158cdecc19e44cc3719e7efd36
Ali Sultanov <ali.sultanov.2024@mail.ru>
2024-02-10 12:53:55 +0300

    Update asyncio.py

--- asyncio.py
+++ asyncio.py
@@ -104,3 +104,36 @@ async def main() -> None:
 # Запуск ассинхронной программы
 if __name__ == '__main__':
     asyncio.run(main())
+
+
+class MyCNNClassifier(nn.Module):
+    def __init__(self, in_c, n_classes):
+        super().__init__()
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(in_c, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+        
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(64 * 28 * 28, 1024),
+            nn.Sigmoid(),
+            nn.Linear(1024, n_classes)
+        )
+
+        
+    def forward(self, x):
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+
+        x = x.view(x.size(0), -1) # Flatten
+        
+        x = self.decoder(x)
+        
+        return x
