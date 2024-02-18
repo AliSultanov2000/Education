8fc163fa84559a95bfb5d980d695ed5b528d66b8
Ali Sultanov <ali.sultanov.2024@mail.ru>
2024-02-18 17:28:52 +0300

    Update merge.py

--- yandex_algorithms/two_pointers/merge.py
+++ yandex_algorithms/two_pointers/merge.py
@@ -5,7 +5,7 @@
 
 def merge(nums1, nums2): 
     merged = [0] * (len(nums1) + len(nums2))
-    first1 = first2 = 0 
+    first1 = first2 = 0  # Два указателя
     for k in range(len(nums1) + len(nums2)):
         if first1 != len(nums1) and (first2 == len(nums2) or nums1[first1] <= nums2[first2]):
             merged[k] = nums1[first1]
