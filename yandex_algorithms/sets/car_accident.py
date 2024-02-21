8ee8e80c7c55fcee1f1ea1518e2119215d31c888
Ali Sultanov <ali.sultanov.2024@mail.ru>
2024-02-21 10:12:50 +0300

    Update car_accident.py

--- yandex_algorithms/sets/car_accident.py
+++ yandex_algorithms/sets/car_accident.py
@@ -9,3 +9,30 @@
 
 # Будем смотреть, является ли показания свидетеля подмножеством номера подозреваемого авто.
 # Если это так, увеличим счётчик сведителей для данного авто на один
+
+
+m = int(input())  # Кол-во Свидетелей
+wits = []  # Свидетели
+for _ in range(m):
+    wits.append(set(input().strip()))
+
+n = int(input())  # Кол-во Номеров
+nums = []  # Список с кортежем: (номер, кол-во свидетелей)
+maxwitcnt = 0
+
+for i in range(n):
+    num = input().strip()
+    numset = set(num)
+    witcnt = 0
+    for wit in wits: 
+        if wit <= numset:  # Если всё множество numset включает в себя wit
+            witcnt += 1
+    nums.append((num, witcnt))
+    maxwitcnt = max(maxwitcnt, witcnt)
+
+
+ans = []  # Ответ с номерами, которые чаще всего встречались (их может быть несколько, если они равны)
+for num in nums: 
+    if num[1] == maxwitcnt:
+        ans.append(num[0])
+print('\n'.join(ans))  # Печатаем через пробел
