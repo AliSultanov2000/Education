be2e5476e299792d53f70449435c86dee9421c40
Ali Sultanov <ali.sultanov.2024@mail.ru>
2024-02-27 16:34:02 +0300

    Create quick_sort.py

---
+++ yandex_algorithms/sorting/quick_sort.py
@@ -0,0 +1,13 @@
+def quick_sort(nums: list[int]):
+    if len(nums) == 0:
+        return nums
+
+    element = nums[0]
+    left = list(filter(lambda x: x < element, nums))
+    center = list(filter(lambda x: x == element, nums))
+    right = list(filter(lambda x: x > element, nums))
+
+    return quick_sort(left) + center + quick_sort(right)
+
+mass = list(map(int, input().split()))
+quick_sort(mass)
