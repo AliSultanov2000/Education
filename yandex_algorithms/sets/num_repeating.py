a4fc23ef7435a7da677881a5f994f3654b5d4e0f
Ali Sultanov <ali.sultanov.2024@mail.ru>
2024-03-28 17:27:55 +0300

    Create num_repeating.py

---
+++ yandex_algorithms/sets/num_repeating.py
@@ -0,0 +1,21 @@
+# Требуется проверить, повторялось ли какое-либо число в последовательности, причём расстояние между
+# повторами не превосходило число k
+# Если всё ок - вернуть YES; Иначе - NO
+
+def sol(nums, k):
+    lastk = set()
+    for i in range(len(nums)):
+        if nums[i] in lastk:
+            return True
+        lastk.add(nums[i])
+        if i >= k:
+            lastk.remove(nums[i - k])
+    return False
+
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+
+if sol(nums, k):
+    print('yes')
+else:
+    print('no')
